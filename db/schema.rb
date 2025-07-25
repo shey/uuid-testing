cdd83ff680998a302d44f29b98491ef613485baa
Sheheryar Sewani <shey@rootly.com>
2025-07-25 17:16:35 -0400

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_25_211207) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_25_211447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
+  create_table "uuid_v4_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "uuid_v7_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+require 'securerandom'
+require 'uuid/v7'
+
+puts "Seeding UUIDv4 records..."
+500_000.times.each_slice(1_000) do |slice|
+  UuidV4Record.insert_all!(
+    Array.new(1_000) { { id: SecureRandom.uuid, name: "V4 Name #{rand(1..999_999)}", created_at: Time.now, updated_at: Time.now } }
+  )
+end
+
+puts "Seeding UUIDv7 records..."
+500_000.times.each_slice(1_000) do |slice|
+  UuidV7Record.insert_all!(
+    Array.new(1_000) { { id: UUID.v7.to_s, name: "V7 Name #{rand(1..999_999)}", created_at: Time.now, updated_at: Time.now } }
+  )
+end
+
+puts "Seeding complete."
